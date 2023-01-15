da3926a0c442d6a6057381e5b12c9f3b797a33fd
Kenny <kenji.maddela01@gmail.com>
2023-01-15 16:31:57 -0500

    Remove color boundaries of the LinkMenu components in the Header and Footer.

--- src/components/layout/link-menu/LinkItem.tsx
+++ src/components/layout/link-menu/LinkItem.tsx
@@ -9,7 +9,7 @@ type Props = {
 const LinkItem = ({ linkText, isSelected, key }: Props) => {
 	return (
 		<li
-			className="bg-purple-500 p-2 shadow-lg rounded-lg duration-200 hover:scale-105 cursor-pointer border border-purple-900 border-opacity-10 text-center font-light "
+			className="p-2 duration-200 hover:scale-110 cursor-pointer text-center font-light "
 			key={key}
 		>
 			{linkText}
