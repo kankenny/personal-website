5c00308b54627911809cbeb2aeb59994e5fb252b
Kenny <kenji.maddela01@gmail.com>
2023-01-16 15:10:46 -0500

    Format the BannerDescription component's contents to cater to mobile

--- src/components/layout/body/banner/Banner.tsx
+++ src/components/layout/body/banner/Banner.tsx
@@ -8,11 +8,11 @@ import portrait from '../../../../assets/home/portrait.png'
 const Banner = () => {
 	return (
 		<SectionContainer twClasses="bg-zinc-100 pb-0 cursor-pointer group">
-			<div className="flex flex-row justify-between items-center">
+			<div className="flex flex-row  items-center">
 				<img
 					src={portrait.src}
 					alt="Kennette James Basco"
-					className="w-[25%] md:w-[35%] min-w-[25%]"
+					className="w-[35%] hidden md:block"
 				/>
 				<BannerDescription />
 			</div>
--- src/components/layout/body/banner/BannerDescription.tsx
+++ src/components/layout/body/banner/BannerDescription.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BannerDescription = () => {
 	return (
-		<div className="w-full text-zinc-900 text-md md:text-3xl space-y-10 text-right p-10 md:pr-0">
+		<div className="w-full text-zinc-900 text-lg md:text-3xl space-y-10 text-center p-10 md:pr-0 md:mx-auto md:text-right">
 			<h1 className="">
 				Hi there! My name is{' '}
 				<p className="group-hover:scale-105 group-hover:underline decoration-zinc-100 group-hover:text-purple-700 group-hover:decoration-purple-700 group-hover:italic group-hover:font-bold duration-300 ">
