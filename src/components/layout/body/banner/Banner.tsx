8ec6f0c96c7982024236d81d6b38ba908ea48eb2
Kenny <kenji.maddela01@gmail.com>
2023-01-16 14:59:01 -0500

    Implement the Banner component

--- src/components/layout/body/banner/Banner.tsx
+++ src/components/layout/body/banner/Banner.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-import portrait from '../../../../assets/home/portrait.png'
-
 import SectionContainer from '../../../ui/SectionContainer'
+import BannerDescription from './BannerDescription'
+
+import portrait from '../../../../assets/home/portrait.png'
 
 const Banner = () => {
 	return (
-		<SectionContainer twClasses="bg-gradient-to-r from-zinc-900 via-zinc-700 to-zinc-500 group pb-0">
+		<SectionContainer twClasses="bg-zinc-100 pb-0 cursor-pointer group">
 			<div className="flex flex-row justify-between items-center">
 				<img
 					src={portrait.src}
 					alt="Kennette James Basco"
-					className="group-hover:opacity-30 duration-700 bottom-0"
+					className="w-[25%] md:w-[35%] min-w-[25%]"
 				/>
+				<BannerDescription />
 			</div>
 		</SectionContainer>
 	)
---
+++ src/components/layout/body/banner/BannerDescription.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const BannerDescription = () => {
+	return (
+		<div className="w-full text-zinc-900 text-md md:text-3xl space-y-10 text-right p-10 md:pr-0">
+			<h1 className="">
+				Hi there! My name is{' '}
+				<p className="group-hover:scale-105 group-hover:underline decoration-zinc-100 group-hover:text-purple-700 group-hover:decoration-purple-700 group-hover:italic group-hover:font-bold duration-300 ">
+					Kennette James Basco
+				</p>
+			</h1>
+			<h2>
+				My enjoyment comes from developing{' '}
+				<p className="group-hover:scale-105 group-hover:underline decoration-zinc-100 group-hover:text-yellow-500 group-hover:decoration-yellow-500 group-hover:italic group-hover:font-bold duration-300 ">
+					dynamic and interactive websites.
+				</p>{' '}
+				I aim to be a
+				<br />{' '}
+				<p className="group-hover:scale-105 group-hover:underline decoration-zinc-100 group-hover:text-purple-700 group-hover:decoration-purple-700 group-hover:italic group-hover:font-bold duration-300 ">
+					Fullstack Software Engineer
+				</p>{' '}
+				one day.
+			</h2>
+		</div>
+	)
+}
+
+export default BannerDescription
