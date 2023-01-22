8c3dd8a11448c893d04ebf9860732980e5ad706f
Kenny <kenji.maddela01@gmail.com>
2023-01-22 15:03:34 -0500

    Add the Banner component for each page

--- src/components/routes/hobbies/Hobbies.tsx
+++ src/components/routes/hobbies/Hobbies.tsx
@@ -1,7 +1,13 @@
+import Body from '@/components/layout/body/Body'
 import React from 'react'
+import Banner from '../home/banner/Banner'
 
 const Hobbies = () => {
-	return <div>Hobbies</div>
+	return (
+		<Body>
+			<Banner />
+		</Body>
+	)
 }
 
 export default Hobbies
--- src/pages/academics/index.tsx
+++ src/pages/academics/index.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/layout/footer/Footer'
 import Header from '@/components/layout/header/Header'
 
 import CHAKRAPETCHFONT from '@/components/ui/ChakraPetchFont'
+import Academics from '@/components/routes/academics/Academics'
 
 const index = () => {
 	return (
@@ -12,6 +13,7 @@ const index = () => {
 				className={`main-web-container flex flex-col min-h-screen ${CHAKRAPETCHFONT.className}`}
 			>
 				<Header />
+				<Academics />
 				<Footer />
 			</div>
 		</main>
