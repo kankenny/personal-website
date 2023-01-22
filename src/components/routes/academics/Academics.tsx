b4e42ad1d89ab1da392bbe65ed8d3639b3b8abf5
Kenny <kenji.maddela01@gmail.com>
2023-01-22 15:42:49 -0500

    Implement the Academics and Hobbies component

--- src/components/routes/academics/Academics.tsx
+++ src/components/routes/academics/Academics.tsx
@@ -1,11 +1,24 @@
-import Body from '@/components/layout/body/Body'
 import React from 'react'
+
+import Body from '@/components/layout/body/Body'
+import Feature from '@/components/layout/feature/Feature'
 import Banner from '../home/banner/Banner'
+import AcademicSummary from './AcademicSummary'
 
 const Academics = () => {
 	return (
 		<Body>
 			<Banner />
+			<Feature
+				img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+				title="test"
+				description="
+	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores! 
+	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores! 
+	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores!"
+				featureType="Not a feature"
+			/>
+			<AcademicSummary />
 		</Body>
 	)
 }
---
+++ src/components/routes/hobbies/HobbySummary.tsx
@@ -0,0 +1,74 @@
+import OverviewCard from '@/components/ui/OverviewCard'
+import SectionContainer from '@/components/ui/SectionContainer'
+import SectionTitle from '@/components/ui/SectionTitle'
+import CardHighlightClass from '@/utils/data-models/CardHighlightClass'
+import React from 'react'
+
+const HobbySummary = () => {
+	const dummyImage = 'https://unsplash.com/photos/sf_1ZDA1YFw'
+
+	const workSummaries = [
+		new CardHighlightClass(
+			dummyImage,
+			'Supplemental Instruction Leader',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student Tutor'
+		),
+		new CardHighlightClass(
+			dummyImage,
+			'Undergraduate Research Assistant',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student'
+		),
+		new CardHighlightClass(
+			dummyImage,
+			'Supplemental Instruction Leadera',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student Tutor'
+		),
+		new CardHighlightClass(
+			dummyImage,
+			'Undergraduate Research Assistants',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student'
+		),
+		new CardHighlightClass(
+			dummyImage,
+			'Supplemental Instruction Leaderd',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student Tutor'
+		),
+		new CardHighlightClass(
+			dummyImage,
+			'Undergraduate Research Assistantf',
+			'Assisted students by conducting biweekly sessions to teach integral calculus',
+			'New York Institute of Technology',
+			'Student'
+		),
+	]
+
+	return (
+		<SectionContainer twClasses="py-20">
+			<SectionTitle sectionTitle="See other passions" />
+			<div className="mx-auto grid grid-cols-1 gap-6 p-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+				{workSummaries.map((workSummary) => (
+					<OverviewCard
+						img={workSummary.img}
+						title={workSummary.title}
+						overview={workSummary.overview}
+						workCompany={workSummary.workCompany}
+						position={workSummary.position}
+						key={workSummary.title}
+					/>
+				))}
+			</div>
+		</SectionContainer>
+	)
+}
+
+export default HobbySummary
