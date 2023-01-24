c0989495296020cfca98cad6e255d33ea43ee862
Kenny <kenji.maddela01@gmail.com>
2023-01-24 17:27:53 -0500

    Change stroke-width to strokeWidth

--- src/pages/hobbies/index.tsx
+++ src/pages/hobbies/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import Footer from '@/components/layout/footer/Footer'
-import Header from '@/components/layout/header/Header'
-
-import CHAKRAPETCHFONT from '@/components/ui/ChakraPetchFont'
 import Hobbies from '@/components/routes/hobbies/Hobbies'
 
 const index = () => {
