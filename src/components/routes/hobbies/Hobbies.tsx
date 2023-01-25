22bd34bc75a7bf6d01591422dbe60ea69928a85f
Kenny <kenji.maddela01@gmail.com>
2023-01-24 23:05:05 -0500

    Remove the Banner component from all pages other than the Home page

--- src/components/routes/hobbies/Hobbies.tsx
+++ src/components/routes/hobbies/Hobbies.tsx
@@ -3,13 +3,18 @@ import React from 'react'
 import Body from '@/components/layout/body/Body'
 import Connect from '@/components/layout/body/connect/Connect'
 import Feature from '@/components/layout/body/feature/Feature'
-import Banner from '../home/banner/Banner'
 import HobbySummary from './HobbySummary'
+import Headline from '@/components/layout/body/headline/Headline'
 
 const Hobbies = () => {
 	return (
 		<Body>
-			<Banner />
+			<Headline
+				headlineTitle="Some Headline"
+				date="Oct 01"
+				description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores! "
+				headlineType="someType"
+			/>
 			<Feature
 				img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
 				title="test"
--- src/components/routes/projects/Projects.tsx
+++ src/components/routes/projects/Projects.tsx
@@ -10,7 +10,12 @@ import Feature from '@/components/layout/body/feature/Feature'
 const Projects = () => {
 	return (
 		<Body>
-			<Banner />
+			<Headline
+				headlineTitle="Some Headline Title"
+				headlineType="Some Type"
+				date="10/29/00"
+				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magni, dignissimos explicabo odit iste, suscipit laborum eos veritatis aut ipsam soluta, alias sapiente! Numquam eaque laudantium ad molestias dicta aliquid"
+			/>
 			<Feature
 				img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
 				title="test"
@@ -19,12 +24,6 @@ const Projects = () => {
 	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores! 
 	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum magnam ullam rem vel exercitationem voluptas itaque unde laboriosam cum asperiores!"
 				featureType="Not a feature"
-			/>
-			<Headline
-				headlineTitle="Some Headline Title"
-				headlineType="Some Type"
-				date="10/29/00"
-				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore magni, dignissimos explicabo odit iste, suscipit laborum eos veritatis aut ipsam soluta, alias sapiente! Numquam eaque laudantium ad molestias dicta aliquid"
 			/>
 			<Connect connectWithMeText="Interested in collaborating with me?" />
 			<ProjectsSummary />
