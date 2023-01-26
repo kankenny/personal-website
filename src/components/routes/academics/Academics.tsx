b28c92ed15485e83ccab07013066502dc53a89a9
Kenny <kenji.maddela01@gmail.com>
2023-01-25 23:07:41 -0500

    Add links to route to the dynamic pages of the Feature components

--- src/components/routes/academics/Academics.tsx
+++ src/components/routes/academics/Academics.tsx
@@ -4,6 +4,7 @@ import Body from '@/components/layout/body/Body'
 import Feature from '@/components/layout/body/feature/Feature'
 import AcademicSummary from './AcademicSummary'
 import Headline from '@/components/layout/body/headline/Headline'
+import Link from 'next/link'
 
 const Academics = () => {
 	return (
@@ -12,12 +13,16 @@ const Academics = () => {
 				headlineTitle="A showcase of passion & interest"
 				description="Discover my academic achievements and see how I turned my passion into a reality. Understand how my interests have guided my educational journey and how they can be an asset to your organization."
 			/>
-			<Feature
-				img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-				title="3.94 Cumulative GPA"
-				description="As a Presidential Scholar awardee, I am proud to have achieved a 3.94 GPA while studying computer science at the New York Institute of Technology. My academic performance is a reflection of my dedication to my studies and my ability to excel in a challenging academic environment. In addition to my coursework, I have also pursued extracurricular interests in machine learning and research, further demonstrating my commitment to my field and my desire to stay current with the latest advancements in technology. I believe that my achievements can bring value to an organization looking for a highly motivated and skilled computer science professional."
-				featureType="Grades"
-			/>
+			<Link href={`academics/3.94-cumulative-gpa`} legacyBehavior>
+				<a href="">
+					<Feature
+						img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+						title="3.94 Cumulative GPA"
+						description="As a Presidential Scholar awardee, I am proud to have achieved a 3.94 GPA while studying computer science at the New York Institute of Technology. My academic performance is a reflection of my dedication to my studies and my ability to excel in a challenging academic environment. In addition to my coursework, I have also pursued extracurricular interests in machine learning and research, further demonstrating my commitment to my field and my desire to stay current with the latest advancements in technology. I believe that my achievements can bring value to an organization looking for a highly motivated and skilled computer science professional."
+						featureType="Grades"
+					/>
+				</a>
+			</Link>
 			<AcademicSummary />
 		</Body>
 	)
--- src/components/routes/hobbies/Hobbies.tsx
+++ src/components/routes/hobbies/Hobbies.tsx
@@ -5,6 +5,7 @@ import Connect from '@/components/layout/body/connect/Connect'
 import Feature from '@/components/layout/body/feature/Feature'
 import HobbySummary from './HobbySummary'
 import Headline from '@/components/layout/body/headline/Headline'
+import Link from 'next/link'
 
 const Hobbies = () => {
 	return (
@@ -13,12 +14,16 @@ const Hobbies = () => {
 				headlineTitle="What tickles my brain and gives me joy"
 				description="Uncover my passions and find out what tickles my brain and gives me joy, by exploring my hobbies. See how my interests have helped me develop skills and knowledge that can be applied in my daily life."
 			/>
-			<Feature
-				img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
-				title="Factorio"
-				description="I am an avid player of the game Factorio, and have a deep passion for the game's design and gameplay. The game's complexity and challenge are endlessly engaging for me, and I enjoy the satisfaction of optimizing my factory's efficiency through automation and solving bottlenecks in resources. I have applied my knowledge of software design concepts such as scalability in my gameplay, specifically in the form of building a main bus and utilizing robotics. Playing Factorio is not just a hobby for me, but a way to constantly improve my problem-solving, critical thinking, and ability to optimize and automate processes."
-				featureType="Not a feature"
-			/>
+			<Link href={`hobbies/factorio`} legacyBehavior>
+				<a>
+					<Feature
+						img="https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+						title="Factorio"
+						description="I am an avid player of the game Factorio, and have a deep passion for the game's design and gameplay. The game's complexity and challenge are endlessly engaging for me, and I enjoy the satisfaction of optimizing my factory's efficiency through automation and solving bottlenecks in resources. I have applied my knowledge of software design concepts such as scalability in my gameplay, specifically in the form of building a main bus and utilizing robotics. Playing Factorio is not just a hobby for me, but a way to constantly improve my problem-solving, critical thinking, and ability to optimize and automate processes."
+						featureType="Not a feature"
+					/>
+				</a>
+			</Link>
 			<HobbySummary />
 			<Connect connectWithMeText="Let's connect to explore our passions together" />
 		</Body>
