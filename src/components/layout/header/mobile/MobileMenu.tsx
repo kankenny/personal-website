65edb30ea4d84753196dcfaa8b682fade5996a03
Kenny <kenji.maddela01@gmail.com>
2023-02-06 22:21:33 -0500

    Add z-index of the MobileMenu component

--- src/components/layout/header/mobile/MobileMenu.tsx
+++ src/components/layout/header/mobile/MobileMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Logo from '@/components/ui/Logo'
 import Links from '../../link-menu/Links'
 
 type Props = {
@@ -9,11 +10,13 @@ type Props = {
 const MobileMenu = ({ isMobileOpen }: Props) => {
 	return (
 		<div
-			className={`bg-darkGray absolute top-0 bottom-0 left-0 flex-col  w-full space-y-3 text-lg text-white items-center h-min p-10 ${
+			className={`z-10 bg-darkGray absolute top-0 bottom-0 left-0 flex-col w-full text-lg text-white items-center h-min p-10 ${
 				!isMobileOpen ? 'hidden' : 'flex'
 			} md:hidden`}
 		>
 			<Links areHeaderLinks={false} />
+			<hr className="my-10 border-t border-1 text-white w-full" />
+			<Logo />
 		</div>
 	)
 }
