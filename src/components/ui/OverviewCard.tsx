cf0ef56c99feadeece4b52d1f0d1aed735607be9
Kennette James Basco <kenji.maddela01@gmail.com>
2023-02-15 23:55:18 -0500

    Refactor hover state of OverviewCard component

--- src/components/ui/OverviewCard.tsx
+++ src/components/ui/OverviewCard.tsx
@@ -30,15 +30,31 @@ const OverviewCard = ({ entry }: Props) => {
 	return (
 		<motion.div variants={cardVariant}>
 			<Card twClasses="rounded-xl bg-black text-white p-2 hover:shadow-xl hover:transform hover:scale-105 duration-200 group cursor-pointer min-h-[30em] flex flex-col justify-between">
-				<div className="space-y-2">
+				<div className="space-y-2 relative text-main">
 					<Image
 						src={entry.img}
 						alt={entry.title}
-						className="h-64 object-cover w-full overflow-hidden rounded-xl shadow-md"
+						className="h-64 object-cover w-full overflow-hidden rounded-xl shadow-md group-hover:blur-sm duration-200 relative"
 					/>
-					<h2 className="text-xl font-bold text-main">
+					<h2 className="text-xl font-bold ">
 						{entry.title}
 					</h2>
+					<svg
+							stroke="currentColor"
+							fill="currentColor"
+							strokeWidth="0"
+							viewBox="0 0 24 24"
+							height="4em"
+							width="4em"
+							className=" opacity-0 group-hover:opacity-100 duration-200 absolute top-1/3 left-[9em]"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<polyline
+								fill="none"
+								strokeWidth="2"
+								points="7 2 17 12 7 22"
+							></polyline>
+						</svg>
 				</div>
 				<div>
 					<div className="pb-2">
@@ -47,7 +63,7 @@ const OverviewCard = ({ entry }: Props) => {
 							{entry.date && <p>{entry.date}</p>}
 						</div>
 					</div>
-					<div className="gap-2 flex justify-between items-center text-main text-sm border-t-gray border-t mt-2 pt-2">
+					<div className="gap-2 items-center text-main text-sm border-t-gray border-t mt-2 pt-2">
 						{entry.position && entry.workCompany && (
 							<div>
 								<span className="font-bold">
@@ -58,22 +74,7 @@ const OverviewCard = ({ entry }: Props) => {
 								</span>
 							</div>
 						)}
-						<svg
-							stroke="currentColor"
-							fill="currentColor"
-							strokeWidth="0"
-							viewBox="0 0 24 24"
-							height="3em"
-							width="3em"
-							className=" opacity-0 group-hover:opacity-100 duration-200"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<polyline
-								fill="none"
-								strokeWidth="2"
-								points="7 2 17 12 7 22"
-							></polyline>
-						</svg>
+						
 					</div>
 				</div>
 			</Card>
