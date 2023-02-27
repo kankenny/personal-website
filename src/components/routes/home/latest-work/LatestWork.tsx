b996748fc2fd61e64509b879064cbe512ee1d53a
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-02-27 13:20:03 -0500

    Remove passing of the isOdd property

--- src/components/routes/home/latest-work/LatestWork.tsx
+++ src/components/routes/home/latest-work/LatestWork.tsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import SectionContainer from '../../../ui/SectionContainer'
-import SectionTitle from '@/components/ui/SectionTitle'
-import LatestWorkList from './LatestWorkList'
+import SectionContainer from "../../../ui/SectionContainer";
+import SectionTitle from "@/components/ui/SectionTitle";
+import LatestWorkList from "./LatestWorkList";
 
 const LatestWork = () => {
-	return (
-		<SectionContainer>
-			<SectionTitle
-				sectionTitle="some of my latest work"
-				isOdd={true}
-			/>
-			<LatestWorkList />
-		</SectionContainer>
-	)
-}
+  return (
+    <SectionContainer>
+      <SectionTitle sectionTitle="some of my latest work" />
+      <LatestWorkList />
+    </SectionContainer>
+  );
+};
 
-export default LatestWork
+export default LatestWork;
