dd558aec125a1f42c0c9ea4b2d7035a88e662446
Kennette Maddela <kennettemaddela@Kennettes-MacBook-Pro.local>
2023-02-27 13:03:40 -0500

    Remove isOdd prop of SectionTitle component

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -2,10 +2,9 @@ import React from "react";
 
 type Props = {
   sectionTitle: string;
-  isOdd?: boolean;
 };
 
-const SectionTitle = ({ sectionTitle, isOdd }: Props) => {
+const SectionTitle = ({ sectionTitle }: Props) => {
   return (
     <div className="p-4">
       <h2 className="uppercase mx-auto text-gray text-sm md:text-md font-bold text-center border-b-gray border-b leading-[.1em] mt-[10px] my-[20px]">
